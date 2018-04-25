b7845d3e23ea25ba5ec056f12d8f74e5625935d3
Arthur Gareginyan <arthurgareginyan@gmail.com>
2018-04-25 06:09:38 +0300

    Plugin updated to version 4.15

--- inc/php/core.php
+++ inc/php/core.php
@@ -20,7 +20,7 @@ add_action( 'init', 'spacexchimp_p001_textdomain' );
  * and inserts a link to the plugin admin page
  */
 function spacexchimp_p001_settings_link( $links ) {
-    $page = '<a href="' . admin_url( 'options-general.php?page=' . SPACEXCHIMP_P001_SLUG . '.php' ) .'">' . __( 'Settings', SPACEXCHIMP_P001_TEXT ) . '</a>';
+    $page = '<a href="' . admin_url( 'options-general.php?page=' . SPACEXCHIMP_P001_SLUG ) . '">' . __( 'Settings', SPACEXCHIMP_P001_TEXT ) . '</a>';
     array_unshift( $links, $page );
     return $links;
 }
