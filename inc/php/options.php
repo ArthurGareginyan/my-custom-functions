6c8dbe93bd517e7cda5c7d9e9d197a65e15717ff
Arthur Gareginyan <arthurgareginyan@gmail.com>
2021-03-08 14:35:18 +0300

    Maintenance: Code formatting improved

--- inc/php/options.php
+++ inc/php/options.php
@@ -22,9 +22,9 @@ function spacexchimp_p001_options() {
 
     // Set default value if option is empty
     $list = array(
+        'enable' => '',
         'hidden_scrollto' => '0',
         'snippets' => '',
-        'enable' => '',
     );
     foreach ( $list as $name => $default ) {
         $array[$name] = !empty( $options[$name] ) ? $options[$name] : $default;
