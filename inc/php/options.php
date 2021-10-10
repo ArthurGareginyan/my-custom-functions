9c0aabc4a02e6abadafc8f481aaaac16c1048796
Arthur Gareginyan <arthurgareginyan@gmail.com>
2021-10-11 00:58:57 +0300

    Maintenance: Processing of options has been improved

--- inc/php/options.php
+++ inc/php/options.php
@@ -15,15 +15,14 @@ function spacexchimp_p001_options() {
     $plugin = spacexchimp_p001_plugin();
 
     // Retrieve options from database
-    $options = get_option( $plugin['settings'] . '_settings' );
+    $array = get_option( $plugin['settings'] . '_settings' );
 
     // Make the "$options" array if the plugin options data in the database is not exist
-    if ( ! is_array( $options ) ) {
-        $options = array();
+    if ( ! is_array( $array ) ) {
+        $array = array();
     }
 
     // Create an array with options
-    $array = $options;
     $list = array(
         'enable' => (boolean) '', // custom
         'hidden_scrollto' => (integer) '0', // _control_hidden
@@ -32,7 +31,7 @@ function spacexchimp_p001_options() {
     foreach ( $list as $name => $default ) {
 
         // Set default value if option is empty
-        $array[$name] = !empty( $options[$name] ) ? $options[$name] : $default;
+        $array[$name] = !empty( $array[$name] ) ? $array[$name] : $default;
 
         // Cast and validate by type of option
         if ( is_string( $default ) === true ) {
